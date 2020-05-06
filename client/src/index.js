1e1f2a1085f975810dfe66307fb6d0a25570af0a
Shujaat Bakhsh <shujaatbakhsh25@gmail.com>
2020-05-05 23:41:51 -0400

    port

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import App from "./components/App";
 
 // Pass your GraphQL endpoint to uri
 const client = new ApolloClient({
-  uri: "http://localhost:4000/"
+  uri: "http://18.219.141.221:4000/"
 });
 
 const ApolloApp = AppComponent => (
